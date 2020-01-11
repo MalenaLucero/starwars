baa9cd1a2c7efd34476e83e5086175638891c9d0
MalenaLucero <luceromalena@hotmail.com>
2020-01-11 19:22:45 -0300

    setting things up

---
+++ main.js
@@ -0,0 +1,42 @@
+let charOneMovies = []
+let charTwoMovies = []
+
+const searchMovie = () =>{
+    const characterOne = document.getElementById('characterOne').value
+    const characterTwo = document.getElementById('characterTwo').value
+    Routes(characterOne)
+        .then(routes=>{
+            let movieTitles = routes.map(route=>{
+                async function name(route){
+                    let name = await fetchMovieName(route)
+                    return name
+                }
+                return name(route)
+            })
+            return movieTitles
+        })
+        .then(res=>{
+            console.log(res)
+        })
+    
+}
+
+async function Routes(character){
+    const movieRoutes = await fetchMovieRoutes(character)
+    return movieRoutes
+}
+
+const fetchMovieRoutes = (character) =>{
+    return fetch('https://swapi.co/api/people/')
+        .then(res=>res.json())
+        .then(res=>{
+            return res.results.find(char => char.name === character).films
+        })
+}
+
+const fetchMovieName = (route) =>{
+    return fetch(route)
+        .then(res=>res.json())
+        .then(res=>res.title)
+}
+
