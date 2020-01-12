19d45042ba2b2a0dfd6694c27a3d967028a95106
MalenaLucero <luceromalena@hotmail.com>
2020-01-12 12:51:56 -0300

    styles added

--- main.js
+++ main.js
@@ -5,27 +5,36 @@ let charTwoMovies = []
 let sharedMovies = []
 
 const searchMovie = () =>{
+    innerHTMLCleaner('errorMessage')
+    innerHTMLCleaner('results')
+    charOneMovies = []
+    charTwoMovies = []
     const characterOne = document.getElementById('characterOne').value
     const characterTwo = document.getElementById('characterTwo').value
     if(characterOne !== '' && characterTwo !== ''){
-        innerHTMLCleaner('errorMessage')
         showLoadingMessage()
         Promise.all([fetchMovies(characterOne, charOneMovies), fetchMovies(characterTwo, charTwoMovies)])
             .then(()=>{
+                sharedMovies = []
                 sharedMovies = getSharedMovies()
+                inputCleaner('characterOne')
+                inputCleaner('characterTwo')
                 hideLoadingMessage()
                 printSharedMovies()
             }) 
+            .catch(()=>{
+                hideLoadingMessage()
+                showErrorMessage('Nothing was found. Please, try again.')
+            })
     }else{
-        showErrorMessage()
+        showErrorMessage('Fill up the search inputs and try again.')
     }
-    
 }
 
 async function fetchMovies(character, moviesArray){
     let movieRoutes = await fetch('https://swapi.co/api/people')
         .then(res=>res.json())
-        .then(res=>res.results.find(e=>e.name.toUpperCase()===character.toUpperCase()).films)
+        .then(res=>res.results.find(e=>e.name.toUpperCase().indexOf(character.toUpperCase())===0).films)
     for(let i=0; i < movieRoutes.length; i++){
         let title = await getMovieTitle(movieRoutes[i])
         moviesArray.push(title)
@@ -49,10 +58,12 @@ const getSharedMovies = () =>{
     return movies
 }
 
+//HTML helpers
+
 const printSharedMovies = () =>{
     const container = document.getElementById('results')
     const title = document.createElement('h2')
-    title.innerText = 'Results:'
+    title.innerText = 'Results'
     container.appendChild(title)
     sharedMovies.forEach(movie=>{
         const listItem = document.createElement('li')
@@ -61,10 +72,10 @@ const printSharedMovies = () =>{
     })
 }
 
-const showErrorMessage = () =>{
+const showErrorMessage = (errorMessage) =>{
     const container = document.getElementById('errorMessage')
     const errorDescription = document.createElement('p')
-    errorDescription.innerText = 'Fill up the search inputs and try again'
+    errorDescription.innerText = errorMessage
     container.appendChild(errorDescription)
 }
 
@@ -81,4 +92,9 @@ const showLoadingMessage = () =>{
 const hideLoadingMessage = () =>{
     const loading = document.getElementById('loading')
     loading.classList.replace('show', 'hide')
+}
+
+const inputCleaner = (elementId) =>{
+    const input = document.getElementById(elementId)
+    input.value =''
 }
