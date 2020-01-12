8e9819339ead187a4f50a27515d7da97cbe81542
MalenaLucero <luceromalena@hotmail.com>
2020-01-11 23:10:52 -0300

    problem solved: arrays are now made up of string instead of promises

--- main.js
+++ main.js
@@ -1,40 +1,51 @@
+//https://swapi.co/api/people/1
+
 let charOneMovies = []
 let charTwoMovies = []
+let sharedMovies = []
 
 const searchMovie = () =>{
-    const characterOne = document.getElementById('characterOne').value
-    const characterTwo = document.getElementById('characterTwo').value
-    Routes(characterOne)
-        .then(routes=>{
-            async () =>{
-                let movies = await routes.map(route=>{
-                    async function name(route){
-                        let name = await fetchMovieName(route)
-                        return name
-                    }
-                    return name(route)
-                })
-                console.log(movies)
-            }
+    //const characterOne = document.getElementById('characterOne').value
+    //const characterTwo = document.getElementById('characterTwo').value
+    Promise.all([fetchMovies(0, charOneMovies), fetchMovies(1, charTwoMovies)])
+        .then(()=>{
+            sharedMovies = getSharedMovies()
+            printSharedMovies()
         })
 }
 
-async function Routes(character){
-    const movieRoutes = await fetchMovieRoutes(character)
-    return movieRoutes
-}
-
-const fetchMovieRoutes = (character) =>{
-    return fetch('https://swapi.co/api/people/')
+async function fetchMovies(num, moviesArray){
+    let movieRoutes = await fetch('https://swapi.co/api/people')
         .then(res=>res.json())
-        .then(res=>{
-            return res.results.find(char => char.name === character).films
-        })
+        .then(res=>res.results[num].films)
+    for(let i=0; i<movieRoutes.length; i++){
+        let title = await getMovieTitle(movieRoutes[i])
+        moviesArray.push(title)
+    }
 }
 
-const fetchMovieName = (route) =>{
+const getMovieTitle = route =>{
     return fetch(route)
         .then(res=>res.json())
         .then(res=>res.title)
 }
 
+const getSharedMovies = () =>{
+    let movies = charOneMovies.filter(charOneMovie=>{
+        let shared = false
+        charTwoMovies.forEach(charTwoMovie=>{
+            if(charTwoMovie === charOneMovie) shared = true
+        })
+        if(shared) return charOneMovie
+    })
+    return movies
+}
+
+const printSharedMovies = () =>{
+    const container = document.getElementById('results')
+    sharedMovies.forEach(movie=>{
+        const listItem = document.createElement('li')
+        listItem.innerText = movie
+        container.appendChild(listItem)
+    })
+}
